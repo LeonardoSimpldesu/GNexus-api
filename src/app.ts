a670a9b9f3cb635a16fd38183735a57b7683f9d9
Leonardo <leonardodesouzaalmeidas@gmail.com>
2024-04-02 21:30:09 -0300

    asd

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,28 @@
+"use strict";
+
 import fastify from 'fastify'
 import { appRoutes } from './routes'
 import { env } from "./env";
 console.log("📃📃📃📃📃📃📃 FOOOODAS");
-const app = fastify()
+const app = fastify({
+    logger: true,
+  })
 
-// app.register(appRoutes)
+app.register(appRoutes)
 
-app
-.listen({
-    host: "0.0.0.0",
-    port: env.PORT,
-})
-.then(() => {
-    console.log("✅ HTTP Server Running!");
-});
-console.log("👌👌👌👌👌👌 PIIITON");
+export default async (req: any, res: any) => {
+    await app.ready();
+    app.server.emit('request', req, res);
+}
+
+// app
+// .listen({
+//     host: "0.0.0.0",
+//     port: env.PORT,
+// })
+// .then(() => {
+//     console.log("✅ HTTP Server Running!");
+// });
+// console.log("👌👌👌👌👌👌 PIIITON");
 
 // export default app
