96f860e3b331cd1469c6fcb1881af0357bc7fe33
Leonardo <leonardodesouzaalmeidas@gmail.com>
2024-04-02 21:15:12 -0300

    AASD

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import fastify from 'fastify'
 import { appRoutes } from './routes'
+import { env } from "./env";
 console.log("📃📃📃📃📃📃📃 FOOOODAS");
 const app = fastify()
 
 // app.register(appRoutes)
 
+app
+.listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+})
+.then(() => {
+    console.log("✅ HTTP Server Running!");
+});
+console.log("👌👌👌👌👌👌 PIIITON");
 export default app
