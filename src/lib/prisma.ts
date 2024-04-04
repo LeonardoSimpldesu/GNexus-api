dd063770e5ecf52dd200f1d1292d39fec3af269b
Leonardo <leonardodesouzaalmeidas@gmail.com>
2024-04-04 09:49:03 -0300

    correct prisma export error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,6 @@
-// import { env } from "@/env";
-// import { PrismaClient } from "@prisma/client";
-// import { Pool } from "pg"
-// import { PrismaPg } from '@prisma/adapter-pg'
+import { env } from "../env";
+import { PrismaClient } from "@prisma/client";
 
-
-// const connectionString = `${process.env.DATABASE_URL}`
-
-// const pool = new Pool({ connectionString })
-
-// const adapter = new PrismaPg(pool)
-// export const prisma = new PrismaClient({ adapter, log: env.NODE_ENV === "dev" ? ["query"] : [], })
+export const prisma = new PrismaClient({
+  log: env.NODE_ENV === "dev" ? ["query"] : [],
+});
