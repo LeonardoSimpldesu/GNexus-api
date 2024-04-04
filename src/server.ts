bacab66e6908b243b350cd9a5faf92f7f0549334
Leonardo <leonardodesouzaalmeidas@gmail.com>
2024-04-04 18:04:37 -0300

    Corret Merge

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-// import app from "./app"
-// import { env } from "./env";
+import { app } from "./app"
+import { env } from "./env";
 
-// console.log("👌👌👌👌👌👌 PIIITON");
-// app
-//   .listen({
-//     host: "0.0.0.0",
-//     port: env.PORT,
-//   })
-//   .then(() => {
-//     console.log("✅ HTTP Server Running!");
-//   });
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("✅ HTTP Server Running!");
+  });
